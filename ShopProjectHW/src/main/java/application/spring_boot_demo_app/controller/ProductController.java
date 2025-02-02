3438e2aaac663360d31081e336e400edf51d2b5f
MacNikSipeykin <niksipeykin@gmail.com>
2025-02-02 10:07:28 +0300

    spring boot homework

---
+++ ShopProjectHW/src/main/java/application/spring_boot_demo_app/controller/ProductController.java
@@ -0,0 +1,45 @@
+package application.spring_boot_demo_app.controller;
+
+
+import application.spring_boot_demo_app.dto.Product;
+import application.spring_boot_demo_app.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProductController {
+  private ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping("/all")
+  public String getAllProducts(Model model){
+    model.addAttribute("products", productService.getAll());
+    return "product_list";
+  }
+
+  @GetMapping("/info/{id}")
+  public String getProductInfo(@PathVariable("id") Integer productId, Model model){
+    model.addAttribute("product", productService.findById(productId));
+    return "product_info";
+  }
+
+  @GetMapping("/add")
+  public String getProductAddForm(){
+    return "product_form";
+  }
+
+  @PostMapping("/add")
+  public String saveProduct(@RequestParam Integer id,
+                            @RequestParam String title,
+                            @RequestParam Integer cost){
+    productService.save(new Product(id, cost, title));
+    return "redirect:all";
+  }
+}
---
+++ ShopProjectHW/src/main/java/application/spring_boot_demo_app/dto/Product.java
@@ -0,0 +1,36 @@
+package application.spring_boot_demo_app.dto;
+
+public class Product {
+  int id, cost;
+  String title;
+
+  public Product(int id, int cost, String title) {
+    this.id = id;
+    this.cost = cost;
+    this.title = title;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getCost() {
+    return cost;
+  }
+
+  public void setCost(int cost) {
+    this.cost = cost;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
